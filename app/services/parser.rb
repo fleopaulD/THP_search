a6d202fcd6819a2cfd77e7c91643aa3f49d185d2
fleopaulD <flo_thp@netc.pl>
2022-03-07 11:53:39 +0100

    add JSON support

--- app/services/parser.rb
+++ app/services/parser.rb
@@ -1,6 +1,10 @@
 class Parser
   UNWANTED_WORDS = %w[de la en le dans les et tu pour des une un est pas ton sur va qui on que te par avec tes bien du il qu ils ta ne peux nous si cette tout]
   
+  def initialize(page)
+    @article = JSON.parse(page)
+  end
+
   def cleanup_string(string)
     cleaned_string = []
     string.split(/ |'/) do |word|
