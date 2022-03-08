bc6b57e2e096f1cf5640f8740b243d923956ac5d
fleopaulD <flo_thp@netc.pl>
2022-03-08 11:36:20 +0100

    Page.save() creation

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,8 @@
 class Page < ApplicationRecord
   has_many :words, through: :titles
+
+  def save(page)
+    new_page = Page.create(url: page.url, signature: page.signature)
+    page.words_in_title.each { |word| Title.create(page: new_page, word: word) }
+  end
 end
