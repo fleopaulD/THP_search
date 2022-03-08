736d4efb7404bb1f2f210d29574f8acb00a2793f
fleopaulD <flo_thp@netc.pl>
2022-03-08 16:38:03 +0100

    implement JSON functionality

--- app/services/scraper.rb
+++ app/services/scraper.rb
@@ -1,9 +1,10 @@
 class Scraper
-  attr_accessor :driver, :all_articles_urls
+  attr_accessor :driver, :all_articles_urls, :wait
 
   def initialize
     options = Selenium::WebDriver::Firefox::Options.new # (args: ['-headless'])
     @driver = Selenium::WebDriver.for(:firefox, options: options)
+    @wait = Selenium::WebDriver::Wait.new(:timeout => 10)
     @all_articles_urls = []
   end
 
@@ -12,6 +13,7 @@ class Scraper
       self.driver.get "https://www.thehackingproject.org/users/sign_in"
       self.driver.find_element(id: "user_email").send_keys ENV["THP_USER_EMAIL"]
       self.driver.find_element(id: "user_password").send_keys ENV["THP_USER_PASSWORD"], :return
+      @wait.until { self.driver.title == "Le tableau de bord | The Hacking Project"}
     rescue => exception
       puts "Error during login to THP website (#{exception})"
       kill()      
@@ -41,8 +43,8 @@ class Scraper
       raw_page = self.driver.find_element(:class_name, "card")
       article[:title] = raw_page.find_element(:class_name, "card-title").text
       article[:url] = url
-      article[:hash] = raw_page.text.hash.to_s
-      article[:subtitles] = raw_page.find_elements(css: ".card-body h2, .card-body h3, .card-body h4, .card-body h5").map { |a| a.text }
+      article[:signature] = raw_page.text.hash.to_s
+      article[:subheadings] = raw_page.find_elements(css: ".card-body h2, .card-body h3, .card-body h4, .card-body h5").map { |a| a.text }
       article[:contents] = raw_page.find_elements(css: ".card-body p, .card-body li").map { |a| a.text }
     rescue => exception
       puts "Error in Scraper.get_content(...) -> #{exception}"
@@ -52,11 +54,22 @@ class Scraper
   end
   
   def kill
-    self.driver.quit()
+    @driver.quit
   end
 
-  def perform
-    self.login()
-    self.get_articles_urls()
+  def get_all_articles(format="object")
+    login
+    get_articles_urls
+    self.all_articles_urls.each do |url|
+      content = get_content(url)
+      if format == "JSON"
+        doc_title = content[:title].delete('/').gsub(/ +/, '_')
+        File.open("./db/json/#{doc_title}.json", 'w') do |f|
+          f.write JSON.generate(content, { array_nl: "\n", object_nl: "\n", indent: '  ' })
+        end
+        puts "File \"./db/json/#{doc_title}.json\" writted."
+      end
+    end
+    kill
   end
 end
