9411e3e7058735429b0e0139161cbe2fe0cdfeff
fleopaulD <flo_thp@netc.pl>
2022-03-08 15:02:52 +0100

    remove Page.save()

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,8 +1,3 @@
 class Page < ApplicationRecord
   has_many :words, through: :titles
-
-  def save(page)
-    new_page = Page.create(url: page.url, signature: page.signature)
-    page.words_in_title.each { |word| Title.create(page: new_page, word: word) }
-  end
 end
