ead3a5a73a43b0a96f9226a341be551ea5f0f2d6
fleopaulD <flo_thp@netc.pl>
2022-03-11 11:39:39 +0100

    implement title and subheadings filter

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,13 +8,15 @@ class SearchController < ApplicationController
   def create
     @user_input = params[:query]
     cleaned_query = cleanup_string(params[:query])
-    @results = nil
+    @results = []
     
     unless cleaned_query.empty?
-      puts "#"*200
-      puts "Cleaned query = #{cleaned_query}"
-      @word = Word.find_by("str LIKE '%#{cleanup_string(params[:query])}%'")
-      @results = @word.pages unless @word.nil?
-    end
+      cleaned_query.split.each do |word|
+        @results << Word.where("str LIKE '%#{word}%'").collect {|a| a.titles.collect { |b| b.page }}
+      end
+      cleaned_query.split.each do |word|
+        @results << Word.where("str LIKE '%#{word}%'").collect {|a| a.subheadings.collect { |b| b.page }}
+      end
+    @results = @results.flatten.uniq.delete_if { |page| page.nil? }
   end
 end
